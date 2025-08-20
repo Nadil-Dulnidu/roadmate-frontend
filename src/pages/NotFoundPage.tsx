75a562b529babb529167e4d1e9bb5ab7b75565e4
Nadil Dulnidu <nzinlud963@gmail.com>
2025-08-20 11:36:04 +0530

    create 404 not found page

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+const NotFoundPage = () => {
+  return (
+    <section className="p-8 container mx-auto">
+      <div className="flex-col items-center justify-center text-center">
+        <h1>404 - Not Found</h1>
+        <p>The page you are looking for does not exist.</p>
+      </div>
+    </section>
+  );
+};
+
+export default NotFoundPage;
